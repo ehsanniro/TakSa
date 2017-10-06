ccf9ab40b4ae04c4c2b047fd96567fd25bbdf0c2
ehsan.niro <ehsan.niro@yahoo.com>
2017-10-06 12:52:43 -0700

    google map

--- app/src/main/java/com/saveh/ehsanniro/taksa/MainActivity.java
+++ app/src/main/java/com/saveh/ehsanniro/taksa/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.places.Places;
 import com.google.android.gms.maps.CameraUpdateFactory;
